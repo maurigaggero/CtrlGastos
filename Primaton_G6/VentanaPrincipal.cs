37aedcc4291b8a568a27ffdb77faa9bbf3e58211
German <rindsberg@hotmail.com>
2019-09-05 15:18:17 -0400

    Modificacion German

---
+++ Primaton_G6/VentanaPrincipal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primaton_G6
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGastos_Click(object sender, EventArgs e)
+        {
+            ControlDeGastos control = new ControlDeGastos();
+            control.Show();
+            this.Hide();
+        }
+
+        private void btnListaSuper_Click(object sender, EventArgs e)
+        {
+            ListaDeSuper lista = new ListaDeSuper();
+            lista.Show();
+            this.Hide();
+        }
+
+        private void btnCuentas_Click(object sender, EventArgs e)
+        {
+            CuentasAPagar cuentas = new CuentasAPagar();
+            cuentas.Show();
+            this.Hide();
+        }
+
+        private void btnGraficos_Click(object sender, EventArgs e)
+        {
+            Graficos graficos = new Graficos();
+            graficos.Show();
+            this.Hide();
+        }
+    }
+}
