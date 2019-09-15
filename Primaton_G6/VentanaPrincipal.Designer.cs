e578875d3ceb29180ace243172f30d8a3409bd7c
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2019-09-14 21:16:28 -0300

    Revert "Corrección"
    This reverts commit 004ce8a03727383c8be69744cd98d48e55ffc292.

--- Primaton_G6/VentanaPrincipal.Designer.cs
+++ Primaton_G6/VentanaPrincipal.Designer.cs
@@ -37,7 +37,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.lblCerrar = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnGastos
@@ -46,7 +45,7 @@
             this.btnGastos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnGastos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGastos.ForeColor = System.Drawing.Color.White;
-            this.btnGastos.Location = new System.Drawing.Point(34, 53);
+            this.btnGastos.Location = new System.Drawing.Point(27, 23);
             this.btnGastos.Name = "btnGastos";
             this.btnGastos.Size = new System.Drawing.Size(213, 180);
             this.btnGastos.TabIndex = 0;
@@ -59,7 +58,7 @@
             this.btnListaSuper.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnListaSuper.BackgroundImage")));
             this.btnListaSuper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnListaSuper.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnListaSuper.Location = new System.Drawing.Point(299, 54);
+            this.btnListaSuper.Location = new System.Drawing.Point(292, 24);
             this.btnListaSuper.Name = "btnListaSuper";
             this.btnListaSuper.Size = new System.Drawing.Size(213, 180);
             this.btnListaSuper.TabIndex = 1;
@@ -71,7 +70,7 @@
             // 
             this.btnCuentas.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCuentas.BackgroundImage")));
             this.btnCuentas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnCuentas.Location = new System.Drawing.Point(34, 292);
+            this.btnCuentas.Location = new System.Drawing.Point(27, 262);
             this.btnCuentas.Name = "btnCuentas";
             this.btnCuentas.Size = new System.Drawing.Size(213, 180);
             this.btnCuentas.TabIndex = 2;
@@ -82,7 +81,7 @@
             // 
             this.btnGraficos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGraficos.BackgroundImage")));
             this.btnGraficos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnGraficos.Location = new System.Drawing.Point(299, 292);
+            this.btnGraficos.Location = new System.Drawing.Point(292, 262);
             this.btnGraficos.Name = "btnGraficos";
             this.btnGraficos.Size = new System.Drawing.Size(213, 180);
             this.btnGraficos.TabIndex = 3;
@@ -95,7 +94,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(41, 236);
+            this.label1.Location = new System.Drawing.Point(34, 206);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(198, 25);
             this.label1.TabIndex = 4;
@@ -107,7 +106,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(309, 236);
+            this.label2.Location = new System.Drawing.Point(302, 206);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(192, 25);
             this.label2.TabIndex = 5;
@@ -119,7 +118,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(47, 474);
+            this.label3.Location = new System.Drawing.Point(40, 444);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(186, 25);
             this.label3.TabIndex = 6;
@@ -131,26 +130,11 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(295, 474);
+            this.label4.Location = new System.Drawing.Point(288, 444);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(221, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Graficos y balances";
-            // 
-            // lblCerrar
-            // 
-            this.lblCerrar.AutoSize = true;
-            this.lblCerrar.BackColor = System.Drawing.Color.Transparent;
-            this.lblCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCerrar.ForeColor = System.Drawing.Color.White;
-            this.lblCerrar.Location = new System.Drawing.Point(502, 9);
-            this.lblCerrar.Name = "lblCerrar";
-            this.lblCerrar.Size = new System.Drawing.Size(31, 29);
-            this.lblCerrar.TabIndex = 8;
-            this.lblCerrar.Text = "X";
-            this.lblCerrar.Click += new System.EventHandler(this.lblCerrar_Click);
-            this.lblCerrar.MouseEnter += new System.EventHandler(this.lblCerrar_MouseEnter);
-            this.lblCerrar.MouseLeave += new System.EventHandler(this.lblCerrar_MouseLeave);
             // 
             // VentanaPrincipal
             // 
@@ -158,8 +142,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(545, 523);
-            this.Controls.Add(this.lblCerrar);
+            this.ClientSize = new System.Drawing.Size(532, 484);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -168,9 +151,7 @@
             this.Controls.Add(this.btnCuentas);
             this.Controls.Add(this.btnListaSuper);
             this.Controls.Add(this.btnGastos);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "VentanaPrincipal";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "VentanaPrincipal";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -187,6 +168,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lblCerrar;
     }
 }
